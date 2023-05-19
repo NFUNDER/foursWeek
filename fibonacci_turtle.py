cfd3f373fd97228b6c9d6e67eccda57355ef7dce
NFUNDER <funder@bigmir.net>
2023-05-19 08:10:08 +0300

    turtle fibonecci new file

---
+++ fibonacci_turtle.py
@@ -0,0 +1,25 @@
+import turtle
+
+turtle.setup(1000, 1000)
+
+def fibonacci(n):
+    if n in (1,2):
+        return 1
+    if not n:
+        return 0
+    return fibonacci (n - 2) + fibonacci(n-1)
+
+t = turtle.Turtle()
+
+for i in range(15):
+    f = fibonacci(i)
+    for i in range(4):
+        t.fd(f)
+        t.lt(90)
+    t.circle(f,180)
+
+turtle.mainloop()
+
+if __name__ == "__main__":
+    main()
+
